d1a7aabdf113cc0f8972dc41fcb721e92aa02d96
Ruan Brandão <ruan.bernardo@gmail.com>
2017-12-15 15:55:28 -0200

    Remove unused code on db schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 20171215165516) do
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
-  create_table "foos", force: :cascade do |t|
-    t.string "bar"
-    t.string "baz"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
