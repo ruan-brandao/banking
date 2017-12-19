43baac7061b2ee27565fa760f7322890e8120b04
Ruan Brandão <ruan.bernardo@gmail.com>
2017-12-19 14:19:55 -0200

    Write action to transfer money between accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -11,4 +11,21 @@ class AccountsController < ApplicationController
       render json: {errors: ['Account does not exist.']}, status: :not_found
     end
   end
+
+  def transfer
+    @account = current_user.account
+    @destination = Account.find(params[:destination_account_id])
+    @amount = params[:amount]
+
+    if @account.transfer(@amount, @destination)
+      render json: {
+        transfered_amount: formatted_currency(@amount),
+        current_balance: formatted_currency(@account.reload.balance)
+      }
+    else
+      render json: {
+        errors: ['The current balance is not enough to make the transfer']
+      }, status: :bad_request
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   get 'accounts/balance'
+  post 'accounts/transfer'
 end
