c93ce10525da453b188f7a44e7462eaac9800caa
Ruan Brandão <ruan.bernardo@gmail.com>
2017-12-19 19:16:32 -0200

    Check amount numericality in AccountsController#transfer

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,10 @@
+require 'irb'
 class AccountsController < ApplicationController
   include AccountsHelper
   before_action :authenticate_user!
   before_action :find_account
-  
+  before_action :check_amount_numericality, only: [:transfer]
+
   def balance
     render json: {balance: formatted_currency(@account.balance)}
   end
@@ -29,6 +31,12 @@ class AccountsController < ApplicationController
     @account = current_user.account
     unless @account.present?
       render json: {errors: ['Account does not exist.']}, status: :not_found
+    end
+  end
+
+  def check_amount_numericality
+    unless params[:amount].is_a? Integer
+      render json: {errors: ['The informed amount is not a number']}, status: :bad_request
     end
   end
 end
--- spec/requests/account_management_spec.rb
+++ spec/requests/account_management_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe 'Account management', type: :request do
       user.account.destroy
       params = {destination_account_id: account.id, amount: 1}
 
-      post '/accounts/transfer', params: params, headers: user.create_new_auth_token
+      post '/accounts/transfer', params: params, headers: user.create_new_auth_token, as: :json
 
       expect(response).to have_http_status(:not_found)
       expect(decode(response.body)['errors']).to eq(['Account does not exist.'])
@@ -42,13 +42,21 @@ RSpec.describe 'Account management', type: :request do
     it 'fails when destination account does not exist' do
       params = {destination_account_id: 6543, amount: 1}
 
-      post '/accounts/transfer', params: params, headers: user.create_new_auth_token
+      post '/accounts/transfer', params: params, headers: user.create_new_auth_token, as: :json
 
       expect(response).to have_http_status(:not_found)
       expect(decode(response.body)['errors']).to eq(["Couldn't find Account with 'id'=6543"])
     end
 
-    it 'fails when amount is not a number'
+    it 'fails when amount is not a number' do
+      params = {destination_account_id: account.id, amount: 'foo'}
+
+      post '/accounts/transfer', params: params, headers: user.create_new_auth_token, as: :json
+
+      expect(response).to have_http_status(:bad_request)
+      expect(decode(response.body)['errors']).to eq(['The informed amount is not a number'])
+    end
+
     it 'fails when amount is greater than current balance'
     it 'debits amount from account and credits amount to destination account'
   end
